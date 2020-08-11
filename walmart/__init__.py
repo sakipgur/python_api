34e5e048cd21c4e466f7df5398fcf0d20aac1676
sakipgur <msakipgur@gmail.com>
2020-08-10 23:43:23 -0500

    flake8

--- walmart/__init__.py
+++ walmart/__init__.py
@@ -1,10 +1,9 @@
 import markdown
 import os
-import shelve
 
 # Import the framework
-from flask import Flask, g
-from flask_restful import Resource, Api, reqparse
+from flask import Flask
+from flask_restful import Resource, Api
 
 # Create an instance of Flask
 app = Flask(__name__)
@@ -12,11 +11,14 @@ app = Flask(__name__)
 # Create the API
 api = Api(app)
 
+
 @app.route("/")
 def index():
 
     # Open the index.md file
-    with open(os.path.dirname(app.root_path) + '/index.md', 'r') as markdown_file:
+    with open(
+            os.path.dirname(app.root_path) + '/index.md', 'r'
+            ) as markdown_file:
 
         # Read the content of the file
         content = markdown_file.read()
@@ -28,10 +30,13 @@ def index():
 class healthz(Resource):
     def get(self):
 
-        stream = os.popen('stat /proc/1 |grep Modify | cut -d. -f1|cut -d " " -f 2,3 ')
+        stream = os.popen(
+                'stat /proc/1 |grep Modify | cut -d. -f1|cut -d " " -f 2,3 '
+                )
         output = stream.read()
         uptime_out = "up since " + output.strip() + " UTC"
 
         return {'status': 'OK', 'version': '0.0.1', 'uptime': uptime_out}, 200
+
 
 api.add_resource(healthz, '/healthz')
